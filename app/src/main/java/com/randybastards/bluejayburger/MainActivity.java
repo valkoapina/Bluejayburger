4ae78a56b135be342eaf960c6848bab9ffef93be
Mikko Bergman <valkoapina@gmail.com>
2015-09-02 21:59:20 +0300

    -Changed main view to full screen -Added a button

--- app/src/main/java/com/randybastards/bluejayburger/MainActivity.java
+++ app/src/main/java/com/randybastards/bluejayburger/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startButtonClick(View view) {
     }
 }
